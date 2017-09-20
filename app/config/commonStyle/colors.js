ddb48c1e7495e59ceeca26b1221e307f268b0c38
Ximena Lasserre <ximena.lasserre@modelit.xyz>
2017-09-20 14:01:47 -0300

    Add categories WIP

--- app/config/commonStyle/colors.js
+++ app/config/commonStyle/colors.js
@@ -1,3 +1,4 @@
 export const textPrimaryColor = '#08415C';
 export const errorColor = '#C33C54';
 export const tealBlueColor = '#388697';
+export const pinkColor ='#EA8888';
--- app/screens/ConfigScreen/ConfigScreen.js
+++ app/screens/ConfigScreen/ConfigScreen.js
@@ -1,7 +1,14 @@
 import React from 'react';
-import { View } from 'react-native';
-import { FormLabel, FormInput, FormValidationMessage } from 'react-native-elements'
-		
+import { View, Modal } from 'react-native';
+import { FormLabel, 
+		FormInput, 
+		FormValidationMessage, 
+		List, 
+		ListItem, 
+		Avatar } from 'react-native-elements'
+import { pinkColor } from '../../config/commonStyle/colors';
+
+import styles from './ConfigScreenStyle';
 
 export default class ConfigScreen extends React.Component {
 
@@ -9,13 +16,21 @@ export default class ConfigScreen extends React.Component {
 		super(props);
 
 		this.state = {
-			maxPerWeek: ''
+			maxPerWeek: '',
+			categories: ['Súper', 'Facturas'],
+			modalVisible: false
 		}
 	}
 
 	static navigationOptions = {
 		title: 'Configuration',
 	};
+
+	openAddCategoryModal() {
+		//this.setState({
+		//	categories: this.state.categories.concat(cat)
+		//});
+	}
 
 	render() {
 		return (
@@ -26,6 +41,30 @@ export default class ConfigScreen extends React.Component {
 						placeholder="5000"
 						onChangeText={(maxPerWeek) => this.setState({maxPerWeek})}/>
 					<FormValidationMessage>Invalid input</FormValidationMessage>
+				</View>
+				<View>
+					<View style={styles.categoryTitle}>
+						<FormLabel>Categorías:</FormLabel>
+						<Avatar
+							small
+							rounded
+							icon={{name: 'add'}}
+							onPress={this.openAddCategoryModal()}
+							activeOpacity={0.7}
+							overlayContainerStyle={{backgroundColor: pinkColor}}
+							containerStyle={{float: 'right'}}
+						/>
+					</View>
+					<List containerStyle={{marginBottom: 20}}>
+					{
+						this.state.categories.map((cat, i) => (
+						<ListItem
+							key={i}
+							title={cat}
+						/>
+						))
+					}
+					</List>
 				</View>
 			</View>
 		);
